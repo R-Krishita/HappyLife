60e78322b98a5eb750b75abac6829baaa67ac63c
RKrishita <krishitaravat12345@gmail.com>
2025-08-23 18:39:43 +0530

    Moved DB Credentials to config.php for security.

--- connection.php
+++ connection.php
@@ -1,6 +1,10 @@
 <?php
 
-    $database= new mysqli("localhost","root","","edoc");
+
+
+    require_once 'config.php';
+
+    $database= new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
